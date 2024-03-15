6d9b6c29c215be32d7970de9a94ee7ad7c0c09c4
raviopq <ravikiranda30@gmail.com>
2024-03-15 16:36:33 +0530

    updating assignment

--- assignment/assignment4/array_puzzle.js
+++ assignment/assignment4/array_puzzle.js
@@ -1,39 +1,30 @@
 /*Problem Description
-You are given an array A of integers of size N.
+Given an array of integers A, find and return the product array of the same size where the ith element of the product array will be equal to the product of all the elements divided by the ith element of the array.
 
-Your task is to find the equilibrium index of the given array
+Note: It is always possible to form the product array with integer (32 bit) values. Solve it without using the division operator.
 
-The equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
 
-If there are no elements that are at lower indexes or at higher indexes, then the corresponding sum of elements is considered as 0.
+Input Format
 
-Note:
+The only argument given is the integer array A.
+Output Format
 
-Array indexing starts from 0.
-If there is no equilibrium index then return -1.
-If there are more than one equilibrium indexes then return the minimum index.
+Return the product array.
+Constraints
 
-constraints:
+2 <= length of the array <= 1000
+1 <= A[i] <= 10
+For Example
 
-1 <= N <= 10^5
--10^5 <= A[i] <= 10^5
-
-eg:
-
-Example Input
 Input 1:
-A = [-7, 1, 5, 2, -4, 3, 0]
-Input 2:
-
-A = [1, 2, 3]
-
-
-Example Output
+    A = [1, 2, 3, 4, 5]
 Output 1:
-3
-Output 2:
+    [120, 60, 40, 30, 24]
 
--1
+Input 2:
+    A = [5, 1, 10, 1]
+Output 2:
+    [10, 50, 5, 50]
 
 */
 
